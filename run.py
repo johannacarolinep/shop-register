614878fa84f41a592b629d5197f75e30e1509af4
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2023-12-17 18:11:59 +0100

    feat: Add functions to get a quantity from user and validate quantity

--- run.py
+++ run.py
@@ -12,6 +12,54 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("shop_register")
 
-inventory = SHEET.worksheet("inventory")
-data = inventory.get_all_values()
-print(data)
+# inventory = SHEET.worksheet("inventory")
+# data = inventory.get_all_values()
+# print(data)
+
+
+def get_quantity():
+    """
+    Get a quantity from the user. Runs a while loop to collect a valid quantity
+    from the user. Needs to be a positive integer number. If quantity larger
+    than 1000, confirm with user.
+    """
+    while True:
+        quantity = input("Enter quantity, a positive integer number, e.g. 17\n")
+
+        if validate_quantity(quantity):
+            quantity = int(quantity)
+            print("Quantity is valid")
+            break
+
+    return quantity
+
+
+
+def validate_quantity(data):
+    """
+    Validates data, returns true if data is a positive integer value.
+    """
+    try:
+        # checks if empty
+        if not data:
+            raise ValueError("Data cannot be empty. Please try again.")
+        
+        # tries to convert to int
+        quantity = int(data)
+
+        # checks if <= 0
+        if quantity <= 0:
+            raise ValueError(f"Quantity must be a positive integer value. Please try again")
+    except ValueError as e:
+        if "invalid literal for int()" in str(e):
+            print("Invalid input. Quantity must be a positive integer. Please try again")
+        else:
+            print(f"Invalid: {e} Please try again.\n")
+        
+        return False
+    
+    return True
+
+quantity = get_quantity()
+print(f"Quantity is: {quantity}")
+print(f"Quantity is of type {type(quantity)}")
