7bb582ab08331a8657b5eca8f4d3fadf2cd3cec8
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2023-12-28 12:15:22 +0100

    refactor: clean up code in validators, add docstrings, remove empty lines

--- validators.py
+++ validators.py
@@ -3,29 +3,53 @@ from datetime import datetime
 
 
 class Validators:
+    """A class of validators for input."""
+
     def validate_not_empty(self, data) -> bool:
         """
-        Validates data, returns true if data exists.
+        Validates data is not empty.
+
+        Parameters:
+        data (string): the user input to check
+
+        Returns:
+        bool: True if data exists, else False
         """
-        try:
-            if not data:
-                raise ValueError("Data cannot be empty.")
-        except ValueError as e:
-            print(f"Invalid: {e} Please try again.\n")
+        if not data:
+            print("Invalid: You submitted an empty input. Please try again.")
             return False
-        return True
+        else:
+            return True
 
     def validate_is_positive(self, data) -> bool:
+        """
+        Validates data is a positive number.
+
+        Parameters:
+        data (string): the user input to check
+
+        Returns:
+        bool: True if data data can be parsed to a positive float, else False
+        """
         try:
             temp_float = float(data)
             if temp_float <= 0:
                 raise ValueError("Data must be a positive value.")
         except ValueError as e:
-            print(f"Invalid: {e} Please try again.\n")
+            print(f"Invalid: {e} Please try again.")
             return False
         return True
 
     def validate_is_int(self, data) -> bool:
+        """
+        Validates data can be parsed to an integer.
+
+        Parameters:
+        data (string): the user input to check
+
+        Returns:
+        bool: True if data can be parsed to an integer, else False
+        """
         try:
             temp_int = int(data)
         except ValueError:
@@ -34,6 +58,15 @@ class Validators:
         return True
 
     def validate_is_float(self, data) -> bool:
+        """
+        Validates data can be parsed to a float.
+
+        Parameters:
+        data (string): the user input to check
+
+        Returns:
+        bool: True if data can be parsed to a float, else False
+        """
         try:
             temp_float = float(data)
         except ValueError:
@@ -43,14 +76,34 @@ class Validators:
             return False
         return True
 
-    def validate_article_existence(self, articleNr, sheet) -> bool:
+    def validate_article_existence(self, article_nr, sheet) -> bool:
+        """
+        Validates if an article nr exists in the first column of a sheet.
+
+        Parameters:
+        article_nr (int): the article number to look for
+        sheet (Worksheet): the worksheet to look in
+
+        Returns:
+        bool: True if article number is found, else False
+        """
         column = sheet.col_values(1)
-        if str(articleNr) in column:
+        if str(article_nr) in column:
             return True
         else:
             return False
 
     def validate_price(self, data) -> bool:
+        """
+        Validates if data (user input) has the right format for a price;
+        is not empty, can be parsed to a float, and is positive.
+
+        Parameters:
+        data (str): the input to check
+
+        Returns:
+        bool: True if input has format of price, else False
+        """
         if (
             self.validate_not_empty(data)
             and self.validate_is_float(data)
@@ -61,6 +114,16 @@ class Validators:
             return False
 
     def validate_quantity(self, data) -> bool:
+        """
+        Validates if data (user input) has the right format for a quantity;
+        is not empty, can be parsed to an integer, and is positive.
+
+        Parameters:
+        data (str): the input to check
+
+        Returns:
+        bool: True if input has format of quantity, else False
+        """
         if (
             self.validate_not_empty(data)
             and self.validate_is_int(data)
@@ -71,6 +134,16 @@ class Validators:
             return False
 
     def validate_article_nr(self, data) -> bool:
+        """
+        Validates if data (user input) has the right format for an article
+        number; is not empty, is positive, and is in the range 1000 - 9999.
+
+        Parameters:
+        data (str): the input to check
+
+        Returns:
+        bool: True if input has format of an article number, else False
+        """
         if (
             self.validate_not_empty(data)
             and self.validate_is_positive(data)
@@ -87,18 +160,26 @@ class Validators:
             return True
 
     def validate_is_name(self, data) -> bool:
+        """
+        Validates if data (user input) has the right format for an article
+        name; is not empty, once stripped of extra spaces, has length of 5-90
+        characters,
+
+        Parameters:
+        data (str): the input to check
+
+        Returns:
+        bool: True if input has format of an article number, else False
+        """
         if self.validate_not_empty(data):
             # Remove trailing whitespaces and extra whitespaces
             cleaned_str = re.sub(r"\s+", " ", data).strip()
-
             try:
-                # Check the maximum length
+                # Check the maximum and minimum length
                 if len(cleaned_str) > 90:
                     raise ValueError("The max length is 90 characters.")
-
                 if len(cleaned_str) < 5:
                     raise ValueError("The minimum length is 5 characters")
-
                 # Check data has letters and optional max 2 digit nr
                 pattern = re.compile(
                     r"(?:[a-zA-Z0-9\s.,!-]*[a-zA-Z]+\s*\d{0,2}\s*"
@@ -107,22 +188,32 @@ class Validators:
                 if bool(pattern.search(cleaned_str)) is False:
                     raise ValueError("Format incorrect.")
             except ValueError as e:
-                print(f"Invalid input: {e}. Please try again.")
+                print(f"Invalid input: {e}.\nPlease try again.")
                 return False
             return True
 
     def validate_is_date(self, date_str) -> bool:
-        # first validate input is not empty
+        """
+        Validates if a string is a valid date: has format YYYY-MM-DD and is a
+        real date (eg not 31st of February).
+
+        Parameters:
+        date_str (str): the string to check
+
+        Returns:
+        bool: True if date_str is a valid date, else False
+        """
         if self.validate_not_empty(date_str):
             # strip trailing whitespaces and check format is correct
             date_str = date_str.strip()
             pattern = re.compile(r"^\d{4}-\d{2}-\d{2}$")
             if pattern.match(date_str):
+                # if correct format, check if it can be parsed to a date object
                 date_format = "%Y-%m-%d"
-                # check if it can be parsed to a date object
                 try:
                     date_to_check = datetime.strptime(date_str, date_format)
                     current_date = datetime.now()
+                    # check that it is not a future date
                     if date_to_check <= current_date:
                         return True
                     else:
@@ -136,6 +227,17 @@ class Validators:
                 return False
 
     def validate_order_nr(self, data) -> bool:
+        """
+        Validates if data (user input) has correct format for an order number:
+        is not empty, can be parsed to an integer, is positive, and in the
+        right range 10000-99999.
+
+        Parameters:
+        date (str): the string to check
+
+        Returns:
+        bool: True if data is a valid order number, else False
+        """
         if (
             self.validate_not_empty(data)
             and self.validate_is_positive(data)
