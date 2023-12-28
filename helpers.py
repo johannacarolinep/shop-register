345eeb1043a42ef4203156941aec5af6d2e5f90f
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2023-12-28 12:24:03 +0100

    refactor: improve docstrings in helpers.py

--- helpers.py
+++ helpers.py
@@ -4,14 +4,23 @@ pretty_table = PrettyTable()
 
 
 def add_row(row, sheet):
-    """Adds a list as a row to a sheet"""
+    """
+    Adds a list as a row to a sheet
+
+    Parameters:
+    row (list): the list to add
+    sheet (Worksheet): the sheet in which to add the row
+    """
     sheet.append_row(row)
 
 
 def display_full_sheet(sheet):
     """
     Gets all data from a sheet, separates headers and rows, and passes
-    them to display_data function
+    them to display_data function.
+
+    Parameters:
+    sheet (Worksheet): the sheet to get the data from
     """
     data = sheet.get_all_values()
     headers = data[0]
@@ -20,7 +29,14 @@ def display_full_sheet(sheet):
 
 
 def display_data(headers, rows):
-    """Displays headers and rows in a table, using pretty table"""
+    """
+    Displays headers and rows in a table, using pretty table
+
+    Parameters:
+    headers (list): list to use as headers in table
+    rows (list): list of lists, where each inner list has the same
+    length as headers
+    """
     pretty_table.field_names = headers
     pretty_table.add_rows(rows)
     print(pretty_table)
